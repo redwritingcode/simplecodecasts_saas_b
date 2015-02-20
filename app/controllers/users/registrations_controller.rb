f84ff8050a38db4b0d80124b4768152ae81c3e18
Red <ladykillington@gmail.com>
2015-02-20 05:37:00 +0000

    Corrected code in User registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   
   def create
     super do |resource|
