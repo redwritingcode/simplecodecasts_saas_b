29cbd767897d4d1df76a395eb8852d22c4ff96b0
Red <ladykillington@gmail.com>
2015-02-20 21:27:57 +0000

    added phone number to profiles and recreated users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220065026) do
+ActiveRecord::Schema.define(version: 20150220200532) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150220065026) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone_number"
   end
 
   create_table "users", force: true do |t|
